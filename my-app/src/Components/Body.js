f1f2a5dd88f5f93b0c9e11977948bde78b7ebece
Suman Bera <suman80bera@gmail.com>
2022-12-26 18:03:14 +0530

    update by suman

--- my-app/src/Components/Body.js
+++ my-app/src/Components/Body.js
@@ -4,8 +4,12 @@ import "./Body.css"
 export default function Body() {
   return (
     <>
-    <div >
-      <img className="background-img" src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80" alt='Img'/> 
+    <div className='container' >
+      <img className="background-img" src="https://wallpaperaccess.com/full/5595849.jpg" alt='Img'/> 
+      <h1 className='center'>MAKE YOU A BOY BUILDER</h1>
+      <p className="text" >We Provide Our best opportunity and qualified trainers for best workout yourself as you want </p>
+      <button className='left-button'>Singup Now</button>
+      <button className='right-button'>View Details</button>
     </div>
     </>
   )
