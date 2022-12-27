62efa3496652b5c0ac629c31e2054d222e1e6310
Suman Bera <suman80bera@gmail.com>
2022-12-27 11:25:58 +0530

    update

--- my-app/src/Components/Body.js
+++ my-app/src/Components/Body.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import "./Body.css"
+import styles from "../Body.module.css"
+
 
 export default function Body() {
   return (
     <>
-    <div className='container' >
-      <img className="background-img" src="https://wallpaperaccess.com/full/5595849.jpg" alt='Img'/> 
-      <h1 className='center'>MAKE YOU A BOY BUILDER</h1>
-      <p className="text" >We Provide Our best opportunity and qualified trainers for best workout yourself as you want </p>
-      <button className='left-button'>Singup Now</button>
-      <button className='right-button'>View Details</button>
+    <div className={styles.container} >
+      <img className={styles.backgroundImg} src="https://wallpaperaccess.com/full/5595849.jpg" alt='Img'/> 
+      <h1 className={styles.center}>MAKE YOU A BODY BUILDER</h1>
+      <p className={styles.text} >We Provide Our best opportunity and qualified trainers for best workout yourself as you want </p>
+      <button className={styles.leftButton}>Singup Now</button>
+      <button className={styles.rightButton}>View Details</button>
     </div>
     </>
   )
--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import "./Header.css"
+import styles from "../Header.module.css"
 
 export default function Header() {
   return (
     <>
-      <nav className="navigation-bar">
-    <img className="logo" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/02/Vector-Gym-Logo-PSD-Template-PNG-Transparent.png" alt='img'/>
-    <a href="/">Home</a>
-    <a href="/">About</a>
-    <a href="/">Pricing</a>
-    <a href="/">Program</a>
-    <button>Register Now</button>
+    <nav className={styles.navigationBar}>
+    <img className={styles.logo} src="https://graphicsfamily.com/wp-content/uploads/edd/2021/02/Vector-Gym-Logo-PSD-Template-PNG-Transparent.png" alt='img'/>
+    <a className={styles.navigationComponent} href="/">Home</a>
+    <a className={styles.navigationComponent} href="/">About</a>
+    <a className={styles.navigationComponent} href="/">Pricing</a>
+    <a className={styles.navigationComponent} href="/">Program</a>
+    <button className={styles.navigationButton}>Register Now</button>
 </nav>
     </>
   )
