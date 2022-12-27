6aa4664174e17e4bac1f2f79fdf8e3c4f3543204
Suman Bera <suman80bera@gmail.com>
2022-12-27 12:12:36 +0530

    update

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <>
     <nav className={styles.navigationBar}>
     <img className={styles.logo} src="https://graphicsfamily.com/wp-content/uploads/edd/2021/02/Vector-Gym-Logo-PSD-Template-PNG-Transparent.png" alt='img'/>
-    <a className={styles.navigationComponent} href="/">Home</a>
+    <a className={styles.navigationComponent}href="/">Home</a>
     <a className={styles.navigationComponent} href="/">About</a>
     <a className={styles.navigationComponent} href="/">Pricing</a>
     <a className={styles.navigationComponent} href="/">Program</a>
